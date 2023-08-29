3b0038aea2213c3c96f3d238b2b491365c7b3045
EphriamHab <ephremhabtamu2015@gmail.com>
2023-08-29 20:27:31 +0300

    api and client modified

--- api/index.js
+++ api/index.js
@@ -1,8 +1,54 @@
 const express = require("express")
+const cors = require("cors");
+const mongoose = require("mongoose");
+const User = require('./models/User');
 const app = express()
-app.post('/register', (req,res)=>{
-res.json('test ok')
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
+
+const bcrypt = require('bcryptjs');
+
+const salt = bcrypt.genSaltSync(10);
+const secret = 'asdfe45we45w345wegw345werjktjwertkj';
+
+app.use(cors({credentials:true,origin:'http://localhost:3000'}));
+mongoose.connect(
+    "mongodb+srv://ephremhabtamu0524:ephrem1234@cluster1.n3bi7va.mongodb.net/mernBlog?retryWrites=true&w=majority"
+    );
+app.use(express.json());
+app.use(cookie-parser());
+
+
+app.post('/register', async(req,res)=>{
+const {username,password} = req.body;
+try {
+const userDoc = await User.create({
+    username,
+    password:bcrypt.hashSync(password,salt),
 })
-app.listen(4000,(req,res)=>{
-    console.log("App is listen on port 400")
+res.json(userDoc);
+} catch (error) {
+    res.status(400).json(error);
+}
+
+app.post('/login',async(req,res)=>{
+const {username,password} = req.body;
+ const userDoc = await User.findOne({username});
+ const passOk = bcrypt.compareSync(password, userDoc.password);
+  if(passOk){
+    jwt.sign({username,id:userDoc._id},secret,{},(err,token)=>{
+     if(err) throw err;
+     res.cookie('token',token).json('ok');
+    })
+  }else{
+    res.status(400).json('wrong credintials')
+  }
 })
+})
+
+app.get('/profile',(req,res)=>{
+  const {token} = req.cookies;
+  
+  res.json(req.cookies)
+})
+app.listen(4000);
--- client/src/components/pages/RegisterPage.js
+++ client/src/components/pages/RegisterPage.js
@@ -1,15 +1,21 @@
 import { useState } from "react"
 
 export default function RegisterPage(){
-    const {username,setUsername} = useState('');
-    const {password,setPassword} = useState('');
+    const [username,setUsername] = useState('');
+    const [password,setPassword] = useState('');
    async function register(e){
        e.preventDefault();
-       await fetch('http:localhost:4000',{
+       
+       const response = await fetch('http://localhost:4000/register',{
         method: 'POST',
         body: JSON.stringify({username,password}),
         headers: {'Content-Type':'application/json'}
        })
+       if(response.status === 200){
+        alert("registration is succesful");
+       }else{
+        alert("registration failed");
+       }
     }
     return(
         <form className="register" onSubmit={register}>
