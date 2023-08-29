79e81162b457b236274dd46a6023efe5535b4cb6
EphriamHab <ephremhabtamu2015@gmail.com>
2023-08-29 13:33:55 +0300

    login and register page

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,24 @@
 
 import './App.css';
+import Header from './components/Header';
+import Layout from './components/Layout';
+import Post from './components/post';
+import {Route,Routes} from "react-router-dom";
+import IndexPage from './components/pages/Indexpage';
+import LoginPage from './components/pages/Loginpage';
+import RegisterPage from './components/pages/RegisterPage';
 
 function App() {
   return (
-    <main>
-      <header>
-      <a href="" className="logo">MyBlog</a>
-      <nav>
-        <a href="">Login</a>
-        <a href="">Register</a>
-      </nav>
-      </header>
-    </main>
+    <Routes>
+      <Route path='/' element={<Layout/>}>
+      <Route index element={<IndexPage />} />
+      <Route path='/login' element={<LoginPage />} />
+      <Route path='/register' element={<RegisterPage/>}/>
+      </Route>
+
+    </Routes>
+
   );
 }
 
---
+++ client/src/components/post.js
@@ -0,0 +1,18 @@
+
+export default function Post(){
+    return(
+        <div className="post">
+        <div className="image">
+        <img src="https://www.womenintech.co.uk/wp-content/uploads/2021/11/Tech-skills-2022-1.png" alt=""/>
+        </div>
+        <div className="text">
+        <h2>Encryption, Bad Bills, and Ripple Effects: How Riana Pfefferkorn Protects the Internet</h2>
+        <p className="info">
+          <a className="author">Ephrem Habtamu</a>
+          <time>2023-08-29</time>
+        </p>
+        <p className="summary">The Internet Resilience Index (IRI) tracks open-source Internet resiliency metrics to support the development of policies and infrastructure to improve Internet</p>
+        </div>
+      </div>
+    );
+}
