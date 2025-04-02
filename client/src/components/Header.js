29d4549d7ca01b8b3e716e93824d9adf25250e74
EphriamHab <ephremhabtamu2015@gmail.com>
2025-04-02 19:07:05 +0300

    base url chnaged

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,55 +1,60 @@
 import { useContext, useEffect } from "react";
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom";
 import { UserContext } from "./Usercontext";
-export default function Header(){
-   const {userInfo,setUserInfo} = useContext(UserContext);
-    useEffect(()=>{
-        fetch('http://localhost:4000/profile',{
-            credentials:'include',
-            headers: {
-              'Accept': 'application/json', // Request JSON response
-            },
-        }).then(response =>{
-           response.json().then(userInfo=>{
-           setUserInfo(userInfo);
-           });
-        });
-    },[]);
-
-    function logout(){
-      fetch('http://localhost:4000/logout',{
-        credentials:'include',
-        method:'POST',
-        headers: {
-          'Accept': 'application/json', // Request JSON response
-        },
-      }).then(() => {
-        setUserInfo(null);
+export default function Header() {
+  const { userInfo, setUserInfo } = useContext(UserContext);
+  useEffect(() => {
+    fetch("https://blog-portfolio-project-alx.vercel.app/profile", {
+      credentials: "include",
+      headers: {
+        Accept: "application/json", // Request JSON response
+      },
+    }).then((response) => {
+      response.json().then((userInfo) => {
+        setUserInfo(userInfo);
       });
-    }
-    const username = userInfo?.username;
-    return(
-        <header>
-        <nav className="navbar">
+    });
+  }, []);
+
+  function logout() {
+    fetch("https://blog-portfolio-project-alx.vercel.app/logout", {
+      credentials: "include",
+      method: "POST",
+      headers: {
+        Accept: "application/json", // Request JSON response
+      },
+    }).then(() => {
+      setUserInfo(null);
+    });
+  }
+  const username = userInfo?.username;
+  return (
+    <header>
+      <nav className="navbar">
         <Link to="/" className="logo">
           MyBlog
         </Link>
-            {username && (
-                <>
-                <Link to="/create" className="create-post">Create new post</Link>
-                <a className="logout" onClick={logout}>Logout</a>
-                </>
-            )}
-            {!username &&(
-              <>
-               
-               <Link to="/login" className="login">Login</Link>
-               <Link to="/register" className="register">Register</Link>
-             </>
-            )}
-
-        </nav>
-        </header>
-    );
-
+        {username && (
+          <>
+            <Link to="/create" className="create-post">
+              Create new post
+            </Link>
+            <a className="logout" onClick={logout}>
+              Logout
+            </a>
+          </>
+        )}
+        {!username && (
+          <>
+            <Link to="/login" className="login">
+              Login
+            </Link>
+            <Link to="/register" className="register">
+              Register
+            </Link>
+          </>
+        )}
+      </nav>
+    </header>
+  );
 }
